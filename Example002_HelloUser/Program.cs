a7bc2eba11b5d1c4be721827a81521902643a5ff
Irina Muravyova <miomir@yandex.ru>
2022-12-15 11:57:08 +0500

    Программа приветствия пользователя

---
+++ Example002_HelloUser/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine();
+Console.Write("Привет, ");
+Console.WriteLine(username);
